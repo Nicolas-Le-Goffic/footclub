9dcb2acd3ddd2b41791767df66ae308a2f628b3a
Nicolas Le-Goffic <legoffic.nicolas@gmail.com>
2025-11-03 08:55:31 +0100

    modifs index.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,13 @@
 <a href = "listeJoueurs.php">Liste des Joueurs</a>
-
+</br>
+</br>
 <a href = "listeEquipes.php">Liste des Equipes</a>
-
+</br>
+</br>
 <a href = "listeEquipesAdverses.php">Liste des Equipes Adverses</a>
-
+</br>
+</br>
 <a href = "listeMembresStaff.php">Liste des Membres du staff</a>
-
+</br>
+</br>
 <a href = "listeRoleEquipeJoueur.php">Liste des affiliations d'un joueur à une équipe et un rôle</a>
